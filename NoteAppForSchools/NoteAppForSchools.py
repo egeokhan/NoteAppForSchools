2e2523134707b1a81125995818675fe4af9bcd4b
Ege Okhan <95880067+egeokhan@users.noreply.github.com>
2022-01-28 11:47:26 +0300

    developed

--- NoteAppForSchools/NoteAppForSchools.py
+++ NoteAppForSchools/NoteAppForSchools.py
@@ -19,10 +19,52 @@ def ReadNote():
     for data in datas:
         print("Student's name: {}\nStudent's sirname: {}\nStudent's school number: {}\nStudent's grade: {}".format(data[0],data[1],data[2],data[3]))
         print("---------------------------------------")
+def ChangeNote():
+    print("---------------------------------------")
+    schoolnumb = int(input("Student's school number: "))
+    newnote = int(input("Student's new grade: "))
+    print("---------------------------------------")
+    print("Old information:")
+    cr.execute("SELECT * FROM infos WHERE number = {}".format(schoolnumb))
+    datastudent = cr.fetchall()
+    print("---------------------------------------")
+    for i in datastudent:
+        print("Name: {}\nSirname: {}\nSchool Number: {}\nNote: {}".format(i[0],i[1],i[2],i[3]))
+        break
+    print("---------------------------------------")
+    cr.execute("UPDATE infos SET note = {} WHERE number = {}".format(newnote,schoolnumb))
+    cr.execute("SELECT * FROM infos WHERE number = {}".format(schoolnumb))
+    newdata = cr.fetchall()
+    print("New information:")
+    print("---------------------------------------")
+    for j in newdata:
+        print("Name: {}\nSirname: {}\nSchool Number: {}\nNote: {}".format(j[0],j[1],j[2],j[3]))
+        break
+    print("---------------------------------------")
+    sure = input("Changes will be saved, are you sure? Y/N: ")
+    if(sure.upper() == "Y"):
+        con.commit()
+        print("Transaction completed successfully")
+        print("---------------------------------------")
+    else:
+        print("The transaction has been cancelled.")
+        print("---------------------------------------")
+def DeleteStudent():
+    schoolnumber = int(input("Student's school number: "))
+    yesno = input("Changes will be saved, are you sure? Y/N: ")
+    if yesno.upper() == "Y":
+        cr.execute("DELETE FROM infos WHERE number = {}".format(schoolnumber))
+        con.commit()
+        print("---------------------------------------")
+        print("Student deleted...")
+    else:
+        print("The transaction has been cancelled.")
+        print("---------------------------------------")
+
 CreateTable()
 while True:
     print("---------------------------------------")
-    action = input("Please select an action:\nPress 1 to enter a student grade\nPress 2 to view student grades\nPress 3 to exit the application\nPress: ")
+    action = input("Please select an action:\nPress 1 to enter a student grade\nPress 2 to view student grades\nPress 3 to change note\nPress 4 to delete a student\nPress 5 to exit the application\nPress: ")
     print("---------------------------------------")
     if action == "1":
         WriteNote()
@@ -43,6 +85,24 @@ while True:
             print("---------------------------------------")
             break
     elif action == "3":
+        ChangeNote()
+        dialog = input("Do you want to return to the menu? Y/N: ")
+        if dialog.upper() == "Y":
+            continue
+        elif dialog.upper() == "N":
+            print("Exiting Application...")
+            print("---------------------------------------")
+            break
+    elif action == "4":
+        DeleteStudent()
+        dialog = input("Do you want to return to the menu? Y/N: ")
+        if dialog.upper() == "Y":
+            continue
+        elif dialog.upper() == "N":
+            print("Exiting Application...")
+            print("---------------------------------------")
+            break
+    elif action == "5":
         print("Exiting Application...")
         print("---------------------------------------")
         break
